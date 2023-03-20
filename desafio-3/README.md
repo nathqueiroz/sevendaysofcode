ab351c276e619e8dc72f879fd28f42fa37b1987f
Nathália Queiróz <101743082+nathqueiroz@users.noreply.github.com>
2023-03-20 18:10:50 -0300

    Adicionando Desafio-3

---
+++ desafio-3/README.md
@@ -0,0 +1,4 @@
+# Desafio 3
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/nathqueiroz/pen/BaOOrQL](https://codepen.io/nathqueiroz/pen/BaOOrQL).
+
---
+++ desafio-3/src/script.js
@@ -0,0 +1,21 @@
+
+var nome = prompt('Qual é o seu nome?');
+
+const area = prompt(nome + ", você quer seguir para área de 'Front-End' ou de 'Back-End'? Digite o nome da área:");
+let linguagem = "";
+if (area === "Front-End"){
+    linguagem = prompt(nome + ", você quer aprender React ou Vue?");
+}
+else if (area === "Back-End"){
+    linguagem = prompt(nome + " você quer aprender C# ou Java?");
+}
+else {
+    alert(nome + " , você não inseriu uma área válida!");
+}
+
+let novaLinguagem = prompt("Tem mais alguma linguagem que você gostaria de aprender? Digite 'sim'");
+while (novaLinguagem == "sim"){
+    let nomeLinguagem = prompt("Qual?");
+    alert(nomeLinguagem + "é realmente é muito legal! Continue se empenhando e tudo dará certo.");
+}
+ alert("Agradeço por participar do nosso Quiz!");
